d9534f9157c5e831dbd6394ee59551efe4452715
Scott Cowe <scott.t.cowe@gmail.com>
2024-04-13 20:49:38 +0100

    Implemented sorting algorithm cuz why not

---
+++ sorting-algorithms/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int nums[10] = { 0, 2, 4, 6, 8, 9, 7, 5, 3, 1 };
+
+  for (int i = 0; i < 10; i++) {
+    printf("%d ", nums[i]);
+  }
+
+  printf("\n");
+
+  bubbleSort(nums, 10);
+
+  for (int i = 0; i < 10; i++) {
+    printf("%d ", nums[i]);
+  }
+
+  printf("\n");
+
+  return 0;
+}
+
+void bubbleSort(int *array, int size) {
+  int n = size;
+  int swapped = 1; // 1 for true, 0 for false
+
+  while (swapped == 1 && n >= 0) {
+    swapped = 0;
+
+    for (int i = 0; i < n - 1; i++) {
+      if (array[i] > array[i + 1]) {
+        int temp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
+        swapped = 1;
+      }
+    }
+
+    n -= 1;
+  }
+}
