7f46378624e0280722293471b540f4d8a2d3058f
Scott Cowe <scott.t.cowe@gmail.com>
2024-04-26 18:45:48 +0100

    completed a simple http server

--- http-server/main.c
+++ http-server/main.c
@@ -1,6 +1,9 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/sendfile.h>
 
 int main() {
   int sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -21,6 +24,14 @@ int main() {
   recv(client_fd, buffer, 256, 0);
 
   printf("%s\n", buffer);
+
+  int resp_fd = open("info", O_RDONLY);
+
+  sendfile(client_fd, resp_fd, 0, 256);
+
+  close(resp_fd);
+  close(client_fd);
+  close(sock);
 
   return 0;
 }
